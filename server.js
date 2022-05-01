508e389d609b11a20743befe97109dee109e67ea
Shubham kumar <s.kumar.s8101997@gmail.com>
2022-05-01 21:11:48 +0530

    updated server.js

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(express.urlencoded({extended: false})) //for receiving the data from cus
 
 app.use(express.json())
 //db-connection
+
 async function initMongoDB() {
    await mongoose.connect(process.env.MONGO_URL, (err) =>{
        if (err) {
@@ -33,10 +34,12 @@ initMongoDB()
 
 //session-config--
 app.use(session({
-    secret: "mySecretKey",
+    secret: process.env.COOKIE_SECRET,
     resave: false,
     store: MongoStore.create({mongoUrl: process.env.MONGO_URL}),
     saveUninitialized: false,
+    clear_interval: 3600,
+    // client:connection.getClient(),
     cookie: {maxAge:1000*60*60*24} //i.e. 24hrs..
 }));
 
@@ -60,9 +63,9 @@ app.use((req,res,next)=>{
 app.use(expressLayout)
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+app.listen(PORT, () =>{
+    console.log( `listening on port ${PORT}`)
+});
 //importing routes
 require('./routes/web')(app)
 
